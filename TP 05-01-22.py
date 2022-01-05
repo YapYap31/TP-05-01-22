deda05b9c7f71f902e7687fea3a1ad2ebf31f6c4
YapYap31 <yapyap880@gmail.com>
2022-01-05 11:45:17 +0100

    modif exo 2

--- TP 05-01-22.py
+++ TP 05-01-22.py
@@ -1,45 +1,94 @@
 #### créer 1 branche github par fonctionnalité ####
 
-### EXERCICE 1 ###
+####################
+#### EXERCICE 1 ####
+####################
 
 import re
 
-nom = input("Quel est votre nom ? \n")
-prenom = input("Quel est votre prénom ? \n")
-annee = int(input("Quelle est votre année de naissance ? \n"))
+# nom = input("Quel est votre nom ? \n")
+# prenom = input("Quel est votre prénom ? \n")
+# annee = int(input("Quelle est votre année de naissance ? \n"))
 
-if annee > 2022 - 6:
-    print("#####################")
-    print("Vous n'êtes pas admis")
-    print("#####################")
-elif annee > 2022 - 12:
-    print("#####################")
-    print("Vous êtes catégorie : poussin")
-    print("#####################")
-elif annee > 2022 - 18:
-    print("#####################")
-    print("Vous êtes catégorie : cadet")
-    print("#####################")
-elif annee > 2022 - 24:
-    print("#####################")
-    print("Vous êtes catégorie : junior")
-    print("#####################")
-elif annee > 2022 - 30:
-    print("#####################")
-    print("Vous êtes catégorie : semi-pro")
-    print("#####################")
-elif annee >= 2022 - 40:
-    print("#####################")
-    print("Vous êtes catégorie : pro")
-    print("#####################")
-else:
-    print("#####################")
-    print("Vous n'êtes pas admis")
-    print("#####################")
+# if annee > 2022 - 6:
+#     print("#####################")
+#     print("Vous n'êtes pas admis")
+#     print("#####################")
+# elif annee > 2022 - 12:
+#     print("#####################")
+#     print("Vous êtes catégorie : poussin")
+#     print("#####################")
+# elif annee > 2022 - 18:
+#     print("#####################")
+#     print("Vous êtes catégorie : cadet")
+#     print("#####################")
+# elif annee > 2022 - 24:
+#     print("#####################")
+#     print("Vous êtes catégorie : junior")
+#     print("#####################")
+# elif annee > 2022 - 30:
+#     print("#####################")
+#     print("Vous êtes catégorie : semi-pro")
+#     print("#####################")
+# elif annee >= 2022 - 40:
+#     print("#####################")
+#     print("Vous êtes catégorie : pro")
+#     print("#####################")
+# else:
+#     print("#####################")
+#     print("Vous n'êtes pas admis")
+#     print("#####################")
 
-print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
-print("#####################")
+# print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
+# print("#####################")
 
-### EXERCICE 2 ###
+####################
+#### EXERCICE 2 ####
+####################
 
+util = int(input("Combien de personnes souhaitez-vous inscrire ? \n"))
+
+mon_dico = {}
+for i in range(util):
+	mon_dico["key{}".format(i)] = input("Nom:")
+
+print(mon_dico)
+
+
+
+# nom = input("Quel est votre nom ? \n")
+# prenom = input("Quel est votre prénom ? \n")
+# annee = int(input("Quelle est votre année de naissance ? \n"))
+
+# if annee > 2022 - 6:
+#     print("#####################")
+#     print("Vous n'êtes pas admis")
+#     print("#####################")
+# elif annee > 2022 - 12:
+#     print("#####################")
+#     print("Vous êtes catégorie : poussin")
+#     print("#####################")
+# elif annee > 2022 - 18:
+#     print("#####################")
+#     print("Vous êtes catégorie : cadet")
+#     print("#####################")
+# elif annee > 2022 - 24:
+#     print("#####################")
+#     print("Vous êtes catégorie : junior")
+#     print("#####################")
+# elif annee > 2022 - 30:
+#     print("#####################")
+#     print("Vous êtes catégorie : semi-pro")
+#     print("#####################")
+# elif annee >= 2022 - 40:
+#     print("#####################")
+#     print("Vous êtes catégorie : pro")
+#     print("#####################")
+# else:
+#     print("#####################")
+#     print("Vous n'êtes pas admis")
+#     print("#####################")
+
+# print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
+# print("#####################")
 
