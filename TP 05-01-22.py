523d93dbbbceee82500499f6d8fa1edfd8499fe8
YapYap31 <yapyap880@gmail.com>
2022-01-05 13:41:39 +0100

    modif

--- TP 05-01-22.py
+++ TP 05-01-22.py
@@ -4,7 +4,7 @@
 #### EXERCICE 1 ####
 ####################
 
-import re
+# import re
 
 # nom = input("Quel est votre nom ? \n")
 # prenom = input("Quel est votre prénom ? \n")
@@ -48,47 +48,40 @@ import re
 
 util = int(input("Combien de personnes souhaitez-vous inscrire ? \n"))
 
-mon_dico = {}
 for i in range(util):
-	mon_dico["key{}".format(i)] = input("Nom:")
+    nom = input("Quel est votre nom ? \n")
+    prenom = input("Quel est votre prénom ? \n")
+    annee = int(input("Quelle est votre année de naissance ? \n"))
 
-print(mon_dico)
+    if annee > 2022 - 6:
+        print("#####################")
+        print("Vous n'êtes pas admis")
+        print("#####################")
+    elif annee > 2022 - 12:
+        print("#####################")
+        print("Vous êtes catégorie : poussin")
+        print("#####################")
+    elif annee > 2022 - 18:
+        print("#####################")
+        print("Vous êtes catégorie : cadet")
+        print("#####################")
+    elif annee > 2022 - 24:
+        print("#####################")
+        print("Vous êtes catégorie : junior")
+        print("#####################")
+    elif annee > 2022 - 30:
+        print("#####################")
+        print("Vous êtes catégorie : semi-pro")
+        print("#####################")
+    elif annee >= 2022 - 40:
+        print("#####################")
+        print("Vous êtes catégorie : pro")
+        print("#####################")
+    else:
+        print("#####################")
+        print("Vous n'êtes pas admis")
+        print("#####################")
 
-
-
-# nom = input("Quel est votre nom ? \n")
-# prenom = input("Quel est votre prénom ? \n")
-# annee = int(input("Quelle est votre année de naissance ? \n"))
-
-# if annee > 2022 - 6:
-#     print("#####################")
-#     print("Vous n'êtes pas admis")
-#     print("#####################")
-# elif annee > 2022 - 12:
-#     print("#####################")
-#     print("Vous êtes catégorie : poussin")
-#     print("#####################")
-# elif annee > 2022 - 18:
-#     print("#####################")
-#     print("Vous êtes catégorie : cadet")
-#     print("#####################")
-# elif annee > 2022 - 24:
-#     print("#####################")
-#     print("Vous êtes catégorie : junior")
-#     print("#####################")
-# elif annee > 2022 - 30:
-#     print("#####################")
-#     print("Vous êtes catégorie : semi-pro")
-#     print("#####################")
-# elif annee >= 2022 - 40:
-#     print("#####################")
-#     print("Vous êtes catégorie : pro")
-#     print("#####################")
-# else:
-#     print("#####################")
-#     print("Vous n'êtes pas admis")
-#     print("#####################")
-
-# print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
-# print("#####################")
+    print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
+    print("#####################")
 
