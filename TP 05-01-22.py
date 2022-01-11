6a70e863234a99c04a3f7e8c44b4c829add0637a
YapYap31 <yapyap880@gmail.com>
2022-01-11 16:24:07 +0100

    exo 4 maj

--- TP 05-01-22.py
+++ TP 05-01-22.py
@@ -6,37 +6,37 @@
 
 # import re
 
-# nom = input("Quel est votre nom ? \n")
-# prenom = input("Quel est votre prénom ? \n")
-# annee = int(input("Quelle est votre année de naissance ? \n"))
+nom = input("Quel est votre nom ? \n")
+prenom = input("Quel est votre prénom ? \n")
+annee = int(input("Quelle est votre année de naissance ? \n"))
 
 # if annee > 2022 - 6:
 #     print("#####################")
-#     print("Vous n'êtes pas admis")
+#     groupe = "Vous n'êtes pas admis"
 #     print("#####################")
 # elif annee > 2022 - 12:
 #     print("#####################")
-#     print("Vous êtes catégorie : poussin")
+#     groupe = "Vous êtes catégorie : poussin"
 #     print("#####################")
 # elif annee > 2022 - 18:
 #     print("#####################")
-#     print("Vous êtes catégorie : cadet")
+#     groupe = "Vous êtes catégorie : cadet"
 #     print("#####################")
 # elif annee > 2022 - 24:
 #     print("#####################")
-#     print("Vous êtes catégorie : junior")
+#     groupe = "Vous êtes catégorie : junior"
 #     print("#####################")
 # elif annee > 2022 - 30:
 #     print("#####################")
-#     print("Vous êtes catégorie : semi-pro")
+#     groupe = "Vous êtes catégorie : semi-pro"
 #     print("#####################")
 # elif annee >= 2022 - 40:
 #     print("#####################")
-#     print("Vous êtes catégorie : pro")
+#     groupe = "Vous êtes catégorie : pro"
 #     print("#####################")
 # else:
 #     print("#####################")
-#     print("Vous n'êtes pas admis")
+#     groupe = "Vous n'êtes pas admis"
 #     print("#####################")
 
 # print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
@@ -73,10 +73,9 @@ import fonctions
 import csv
 from datetime import date
 
-# with open(f'{date.today().year}-{date.today().month}-{date.today().day}.csv', 'a', newline='') as csvfile:
-#     spamwriter = csv.writer(csvfile, delimiter=' ')
-#     spamwriter.writerow(['Bonjour'] * 5 + ['Salut'])
-#     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+with open(f'inscrits-{date.today().year}-{date.today().month}-{date.today().day}.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=':')
+    spamwriter.writerow([f'{groupe}, f'{nom}', f'{prenom}', f'{str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr'])
 
 
 ####################
@@ -84,3 +83,17 @@ from datetime import date
 ####################
 
 
+# on peut utiliser la bibliothèque pandas mais ici on va rester sur import csv
+
+fout=open("inscrits_total.csv","a")
+# first file:
+for line in open("sh1.csv"):
+    fout.write(line)
+# now the rest:    
+for num in range(2,201):
+    f = open("sh"+str(num)+".csv")
+    f.next() # skip the header
+    for line in f:
+         fout.write(line)
+    f.close() # not really needed
+fout.close()
